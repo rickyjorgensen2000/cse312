e5ae2c7a78370b6b8ea84d72d29dccc9ce090a50
jmmoran4 <jmmoran@buffalo.edu>
2022-11-14 12:32:41 -0500

    added startGame, and login functions for js

--- flaskr/actionsJS/actions.js
+++ flaskr/actionsJS/actions.js
@@ -4,11 +4,14 @@ function startGame() {
     request.open('GET', startPath);
     request.send();
 }
-
+// called when user clicks login button
 function login() {
     const request = new XMLHttpRequest()
-    const nextPath = '/home'
-    request.open('GET', nextPath);
+    let username = document.getElementById('login-box');
+    username.focus();
+    const nextPath = '/'
+    request.open('POST', nextPath);
     request.send();
 }
+
 
