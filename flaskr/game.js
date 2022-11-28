4804237bcd76740df74132d9d607b4629fa1143d
jmmoran4 <jmmoran@buffalo.edu>
2022-11-28 12:15:11 -0500

    add games.js

---
+++ flaskr/game.js
@@ -0,0 +1,25 @@
+// Handle in game actions...
+// Buttons will be linked to this file
+
+const socket = new WebSocket('ws://' + window.location.host + '/websocket');
+
+let webRTCConnection;
+let player;
+socket.onmessage = function (ws_message) {
+    const message = JSON.parse(ws_message.data);
+    const messageType = message.messageType
+    console.log("reached js")
+
+
+}
+
+function triggerButton (buttonID) {
+    let button = document.getElementById(buttonID);
+    if (player === 'X') {
+        button.innerText = 'X';
+    }
+    if (player === 'O') {
+        button.innerText = 'O';
+    }
+    button.style.visibility = 'visible'
+}
