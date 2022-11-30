cf20579bd9e691166bb2386c8acf999a065db4cd
jmmoran4 <jmmoran@buffalo.edu>
2022-11-30 12:10:05 -0500

    added more changes to game logic

--- flaskr/static/game.js
+++ flaskr/static/game.js
@@ -1,11 +1,14 @@
 // Handle in game actions...
 // Buttons will be linked to this file
 
+
 const socket = new WebSocket('ws://' + window.location.host + '/websocket');
 
 let webRTCConnection;
 let player = '';
 let state = 0;
+let myMoves = [];
+let oppMoves = [];
 // Wait to establish websocket connection
 socket.onopen = function (e) {
     alert('Connection Established')
@@ -18,9 +21,14 @@ socket.onmessage = function (ws_message) {
     const messageType = message.messageType
     // assign player as 'X' or 'O'
     // assign the current state to determine who's move it is
-    player = message['state'];
-    if (player === 'X'){
-        state = 1;
+    if(messageType === 'assignPlayer') {
+        player = message['state'];
+        if (player === 'X') {
+            state = 1;
+        }
+    }
+    else if (messageType === 'move'){
+        update_opponent_move(message['buttonID'])
     }
     console.log("reached js")
 }
@@ -29,6 +37,7 @@ socket.onmessage = function (ws_message) {
 function triggerButton (buttonID) {
     if (state) {
         let button = document.getElementById(buttonID);
+        myMoves.push(buttonID)
         if (player === 'X') {
             button.style.opacity = '1';
             button.innerText = 'X';
@@ -40,4 +49,24 @@ function triggerButton (buttonID) {
         state = 0;
 
     }
+}
+
+function update_opponent_move (buttonID) {
+    let button = document.getElementById(buttonID);
+    oppMoves.push(buttonID)
+    switch (player) {
+        case 'X':
+            button.style.opacity = '1';
+            button.innerText = 'O';
+            break
+        case 'O':
+            button.style.opacity = '1';
+            button.innerText = 'X';
+            break
+    }
+    state = 1;
+}
+// check if a player has won
+function check_game_state(myMoves, oppMoves) {
+
 }
