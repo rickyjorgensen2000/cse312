7aea588f16631b9d2ffbef118cb3a79af7de1bc0
Deepak Joseph <deepakjoseph23@gmail.com>
2022-11-30 10:58:07 -0500

    added path to game screens in navbar

--- flaskr/main.py
+++ flaskr/main.py
@@ -20,3 +20,7 @@ def profile():
     users_data = db.check_for_user(current_user.username)
     return render_template('profile.html', name = current_user.username, wins = users_data.get('wins'), losses = users_data.get('loss'))
 
+@main.route('/game')
+@login_required
+def game():
+    return render_template('eric_html_files/screen_one.html')
