d1f355dcffd537a47a554b12cba499a7f3956141
Richard Prosser <rdprosser@icloud.com>
2022-11-30 19:17:11 -0500

    Websockets now working, but there is no waiting room yet

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,11 +3,17 @@ try:
     from flask import request
     from flask_login import login_required, current_user
     import flaskr.db as db
+    import flaskr.globals as globals
+    from flask_socketio import emit
+    import flask_socketio
     import sys
 except Exception as e:
     print(" Some pacakages are missing: {}".format(e))
 
 main = Blueprint('main', __name__)
+
+global counter
+counter = 0
 
 
 @main.route('/')
@@ -47,3 +53,25 @@ def leaderboard():
         record.append(all_users_data_dict[key])
 
     return render_template('leaderboard.html', win_loss_list = record)
+
+
+@globals.socketsio.on('connect')
+def test_connect(auth):
+    global counter
+    emit('my response', {'data': 'Connected'})
+    if counter % 2 == 0:
+        emit('state and player', {'State': 1, 'Player': 'X'})
+    else:
+        emit('state and player', {'State': 0, 'Player': 'O'})
+
+    print("COUNTER: " + str(counter), file=sys.stderr)
+    counter += 1
+    sid = request.sid
+    print("MADE IT HERE: " + sid, file=sys.stderr)
+
+    @globals.socketsio.on('player move')
+    def test_messages(msg):
+        print("MADE IT HERE: " + str(msg), file=sys.stderr)
+        emit('opponent move', msg, broadcast=True, include_self=False)
+
+
